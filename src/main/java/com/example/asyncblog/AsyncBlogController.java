d9ecaf244f227c5abb101855bcaa752b691f57bd
Bradley Trager <bradley.trager@cengage.com>
2020-03-05 12:37:57 -0500

    Make controller methods async.

--- src/main/java/com/example/asyncblog/AsyncBlogController.java
+++ src/main/java/com/example/asyncblog/AsyncBlogController.java
@@ -1,11 +1,14 @@
 package com.example.asyncblog;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
+import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -18,14 +21,14 @@ public class AsyncBlogController {
     }
 
     @GetMapping("/blog/{id}")
-    public Blog getBlog(@PathVariable Long id) throws HttpClientErrorException.NotFound {
-        return asyncBlogService.findById(id)
+    public Callable<Blog> getBlog(@PathVariable Long id) throws HttpClientErrorException.NotFound {
+        return () -> asyncBlogService.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(
                 HttpStatus.NOT_FOUND, "Blog Not Found"));
     }
 
     @PostMapping("/blog")
-    public Blog addBlog(@RequestBody Blog blog) {
-        return asyncBlogService.save(blog);
+    public Callable<Blog> addBlog(@RequestBody Blog blog) {
+        return () -> asyncBlogService.save(blog);
     }
 }
